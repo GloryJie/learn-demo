62ba7add96fb9200a86c5b185a8b9eff69c2acd9
GloryJie <GloryJie@163.com>
2019-12-08 22:18:50 +0800

    commit

--- juc-thread-demo/src/main/java/top/gloryjie/learn/juc/MultiSync/CyclicBarrierDemo.java
+++ juc-thread-demo/src/main/java/top/gloryjie/learn/juc/MultiSync/CyclicBarrierDemo.java
@@ -18,8 +18,15 @@ public class CyclicBarrierDemo {
         ExecutorService executor = Executors.newFixedThreadPool(1);
 
         // 因为CyclicBarrier是同步调用回调函数才执行唤醒线程,所以此处使用线程池来执行回调任务
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(2, () ->
-                executor.execute(() -> System.out.println("combine product with component 1 and 2 done")));
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(2, () -> {
+            System.out.println("回调线程: " + Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            executor.execute(() -> System.out.println("combine product with component 1 and 2 done"));
+        });
 
         new Thread(() -> {
             while (true) {
@@ -27,12 +34,14 @@ public class CyclicBarrierDemo {
                     TimeUnit.SECONDS.sleep(2);
                     System.out.println("build component 1 done");
                     cyclicBarrier.await();
+                    // 只有cyclicBarrier 为0 时,才会执行下面的代码
+                    System.out.println("clean component 1 done");
                 } catch (InterruptedException | BrokenBarrierException e) {
                     e.printStackTrace();
                     break;
                 }
             }
-        }).start();
+        }, "Build-Thread-1").start();
 
         new Thread(() -> {
             while (true) {
@@ -40,11 +49,13 @@ public class CyclicBarrierDemo {
                     TimeUnit.SECONDS.sleep(4);
                     System.out.println("build component 2 done");
                     cyclicBarrier.await();
+                    // 只有cyclicBarrier 为0 时,才会执行下面的代码
+                    System.out.println("clean component 2 done");
                 } catch (InterruptedException | BrokenBarrierException e) {
                     e.printStackTrace();
                     break;
                 }
             }
-        }).start();
+        }, "Build-Thread-2").start();
     }
 }
